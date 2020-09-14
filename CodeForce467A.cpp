893c3ba69bf79789941c8078d2f10791dac4d26f
Taj Sapra <sapra.taj@live.com>
2020-09-14 10:56:07 +0530

    467A done

--- CodeForce467A.cpp
+++ CodeForce467A.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int n,ans=0;
     cin>>n;
-    for (int i = 0; i < n; i++)   {
+    for (int i = 0; i < n; i++) {
         int capacity, occupied;
         cin>>occupied>>capacity;
         if (capacity-occupied>=2&&occupied>=0&&capacity>=0){
